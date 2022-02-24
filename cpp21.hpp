ef82c939c344366eb44aa7e062cc3ce62c9e6a71
helixd-2k18 <helixd.2k18@gmail.com>
2022-02-25 04:02:07 +0800

    improved bucket and shared vector

--- cpp21.hpp
+++ cpp21.hpp
@@ -284,70 +284,6 @@ __declspec(align(0)) class void_t { public:
 
 
 
-    /* 
-        // Official version of vector from descriptors of Ramen...
-        // This class reusing free cells, instead erase or delete...
-    */
-
-    template<class T = void_t, template<class Ts = T> class V = std::vector> class bucket { protected: 
-        V<T> used = {};
-        V<uintptr_t> free = {};
-    
-    public: 
-        // 
-        inline bucket(V<T> used = {}, V<uintptr_t> free = {}) : used(used), free(free) {
-            
-        };
-
-        // 
-        inline decltype(auto) add(T const& e) {
-            uintptr_t index = 0u;
-            if (free.size() > 0) {
-                used[index = free.back()] = e;
-                free.pop_back();
-            } else {
-                index = free.size();
-                used.push_back(e);
-            };
-            return index;
-        };
-
-        // 
-        inline decltype(auto) removeByIndex(uintptr_t const& idx) {
-            decltype(auto) last = used.size()-1;
-            if (idx <= last) {
-                used[idx] = {};
-                if (idx == last) { used.resize(last); } else { free.push_back(idx); };
-            };
-            return *this;
-        };
-
-        //
-        inline decltype(auto) removeByValue(T const& element = {}) {
-            //decltype(auto) found = this->used.find();
-            //return this->removeByIndex((found == this->used.end()) ? std::distance(used.begin(), found) : this->used.size());
-            return this->removeByIndex(std::distance(used.begin(), this->used.find()));
-        };
-
-        //
-        inline decltype(auto) remove(uintptr_t const& idx) {
-            return this->removeByIndex(idx);
-        };
-
-        //
-        inline decltype(auto) operator[](uintptr_t const& index) { return used[index]; };
-        inline decltype(auto) operator[](uintptr_t const& index) const { return used[index]; };
-
-        //
-        inline decltype(auto) clear() { used.clear(); free.clear(); used.resize(0u); free.resize(0u); };
-        inline decltype(auto) size() { return used.size(); };
-        inline decltype(auto) data() { return used.data(); };
-        inline decltype(auto) data() const { return used.data(); };
-
-        //
-        inline operator T*() { return used.data(); };
-        inline operator T const*() const { return used.data(); };
-    };
 
 
     /* 
@@ -754,7 +690,7 @@ __declspec(align(0)) class void_t { public:
         };
 
         //
-        inline shared_vector(V<T> const& vect = {}) : vect(std::make_shared<V>(vect.begin(), vect.end())) {
+        inline shared_vector(V<T> const& vect = {}) : vect(std::make_shared<V<T>>(vect.begin(), vect.end())) {
             
         };
 
@@ -785,9 +721,107 @@ __declspec(align(0)) class void_t { public:
         // 
         inline decltype(auto) assign(St const& v) { vect = v; return *this; };
         inline decltype(auto) assign(Vt const& v) { *vect = v; return *this; };
+
+        // 
         inline decltype(auto) operator=(St const& v) { return this->assign(v); };
         inline decltype(auto) operator=(Vt const& v) { return this->assign(v); };
+
+        //
+        inline decltype(auto) get() { return vect.get(); };
+        inline decltype(auto) get() const { return vect.get(); };
     };
+
+
+
+    /*
+        // Official version of vector from descriptors of Ramen...
+        // This class reusing free cells, instead erase or delete...
+    */
+
+    template<class T = void_t, template<class Ts = T> class Vm = std::vector, template<class Ts = T, template<class Ty = Ts> class Va = Vm> class Vs = cpp21::shared_vector> class bucket {
+    protected:
+      Vs<T, Vm> used = {};
+      Vs<uintptr_t, Vm> free = {};
+
+    public:
+      // 
+      inline bucket(std::vector<T> used = {}, Vm<uintptr_t> free = {}) : used(used), free(free) {
+
+      };
+
+      // 
+      inline bucket(Vs<T> used = {}, Vs<uintptr_t> free = {}) : used(used), free(free) {
+
+      };
+
+      // 
+      inline decltype(auto) add(T const& e) {
+        uintptr_t index = 0u;
+        if (free.size() > 0) {
+          used[index = free.back()] = e;
+          free.pop_back();
+        }
+        else {
+          index = free.size();
+          used.push_back(e);
+        };
+        return index;
+      };
+
+      // 
+      inline decltype(auto) removeByIndex(uintptr_t const& idx) {
+        decltype(auto) last = used->size() - 1;
+        if (idx <= last) {
+          used[idx] = {};
+          if (idx == last) { used.resize(last); }
+          else { free.push_back(idx); };
+        };
+        return *this;
+      };
+
+      //
+      inline decltype(auto) removeByValue(T const& element = {}) {
+        //decltype(auto) found = this->used.find();
+        //return this->removeByIndex((found == this->used.end()) ? std::distance(used.begin(), found) : this->used.size());
+        return this->removeByIndex(std::distance(used.begin(), this->used.find()));
+      };
+
+      //
+      inline decltype(auto) remove(uintptr_t const& idx) {
+        return this->removeByIndex(idx);
+      };
+
+      //
+      inline operator Vs<T>& () { return used; };
+      inline operator Vs<T> const& () const { return used; };
+
+      //
+      inline operator Vm<T>& () { return *used; };
+      inline operator Vm<T> const& () const { return *used; };
+
+      //
+      inline decltype(auto) operator[](uintptr_t const& index) { return used->at(index); };
+      inline decltype(auto) operator[](uintptr_t const& index) const { return used->at(index); };
+
+      //
+      inline decltype(auto) clear() { used->clear(); free.clear(); used.resize(0u); free.resize(0u); };
+      inline decltype(auto) size() { return used->size(); };
+      inline decltype(auto) data() { return used->data(); };
+      inline decltype(auto) data() const { return used->data(); };
+
+      //
+      inline operator T* () { return used->data(); };
+      inline operator T const* () const { return used->data(); };
+
+      //
+      inline decltype(auto) operator->() { return used.get(); };
+      inline decltype(auto) operator->() const { return used.get(); };
+
+      //
+      inline decltype(auto) operator*() { return used.get(); };
+      inline decltype(auto) operator*() const { return used.get(); };
+    };
+
 
 
     /* 
