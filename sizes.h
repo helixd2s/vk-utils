59bfce5888b617424687af503db881e2ffb353e2
helixd-2k18 <helixd.2k18@gmail.com>
2022-06-03 08:08:07 +0800

    disable unicode

--- sizes.h
+++ sizes.h
@@ -8,13 +8,6 @@
 #define VULKAN_SIZES_H
 
 //
-#ifdef _UNICODE
-#ifndef UNICODE
-#define UNICODE
-#endif
-#endif
-
-//
 #ifdef _WIN32
 #ifndef VK_USE_PLATFORM_WIN32_KHR
 #define VK_USE_PLATFORM_WIN32_KHR
